e2882f8f7ae3d12f54075cf6df26b8b4ebfd8eea
Vytautas Klumbys <Vytautas.Klumbys@adform.com>
2015-08-10 22:10:33 +0300

    sources

---
+++ SandrosUi/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SandrosUi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] files = Directory.GetFiles("E:\\PUB","*", SearchOption.AllDirectories);
+
+            List<int> skaiciai = new List<int>();
+
+            ListBox.ObjectCollection x = listBox1.Items;
+
+            foreach (string file in files)
+            {
+                listBox1.Items.Add(file);
+            }
+        }
+    }
+}
